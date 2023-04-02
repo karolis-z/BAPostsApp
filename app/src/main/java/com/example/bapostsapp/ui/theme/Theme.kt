9791b363fea939d2ac7f0e441159d58de9d0abcc
zavec <zaveckas.k@gmail.com>
2023-04-02 17:08:00 +0300

    Change status bar and navigation bar colors to surface color to have a cleaner look.
    Added accompanist's systemUiController dependency.

--- app/src/main/java/com/example/bapostsapp/ui/theme/Theme.kt
+++ app/src/main/java/com/example/bapostsapp/ui/theme/Theme.kt
@@ -1,19 +1,11 @@
 package com.example.bapostsapp.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.LocalView
-import androidx.core.view.ViewCompat
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorScheme = darkColorScheme(
     primary = md_theme_dark_primary,
@@ -94,13 +86,10 @@ fun BAPostsAppTheme(
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
-    val view = LocalView.current
-    if (!view.isInEditMode) {
-        SideEffect {
-            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
-        }
-    }
+
+    val systemUiController = rememberSystemUiController()
+    systemUiController.setSystemBarsColor(color = colorScheme.surface)
+    systemUiController.setNavigationBarColor(color = colorScheme.surface)
 
     MaterialTheme(
         colorScheme = colorScheme,
